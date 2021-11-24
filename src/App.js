80d2831661da696145e0865be127e672f3115b44
Darania Muralitharan <90543261+DanaMurali@users.noreply.github.com>
2021-11-24 19:25:01 +0000

    added some button animation and changed the design around a bit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import BucketListContainer from './container/BucketListContainer';
 function App() {
   return (
     <div className="App">
+      <h1>Countries To Visit Before You Kick The Bucket</h1>
       <BucketListContainer/>
     </div>
   );
